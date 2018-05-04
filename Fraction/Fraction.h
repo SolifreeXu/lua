b344376444d230ce2f7787774cc748ff7e5508dd
xucongandxuchong <2592419242@qq.com>
2018-05-04 20:31:22 +0800

    tools
    原创工具类

--- Fraction/Fraction.h
+++ Fraction/Fraction.h
@@ -1,26 +1,34 @@
 #pragma once
 
 #include <iostream>
+#include <exception>
 
 class Fraction
 {
 	long m_iMolecule;
-	long m_iDenominator;
+	long m_iDenominator;	// 分母总是正整数
 
 	friend std::ostream& operator<<(std::ostream &os, const Fraction &fraction);
 	friend std::istream& operator>>(std::istream &is, Fraction &fraction);
 public:
-	Fraction(long molecule = 0, long denominator = 1) \
-		:m_iMolecule(molecule), m_iDenominator(denominator){}
+	Fraction(long molecule = 0, long denominator = 1) throw(std::invalid_argument);
 	Fraction(const Fraction &fraction) \
 		:m_iMolecule(fraction.m_iMolecule), m_iDenominator(fraction.m_iDenominator){}
-	Fraction operator+(Fraction fraction) const;
-	Fraction operator-(Fraction fraction) const;
-	Fraction operator*(Fraction fraction) const;
-	Fraction operator/(Fraction fraction) const;
-	bool operator==(const Fraction &fraction) const;
-	Fraction& operator=(Fraction &&fraction);
-	operator double(){ return (double)m_iMolecule / m_iDenominator; }
-	static unsigned maxCommonDivisor(unsigned first, unsigned second);
-	static unsigned minCommonMultiple(unsigned first, unsigned second);
+	friend Fraction operator+(const Fraction &left, Fraction right) noexcept;
+	friend Fraction operator-(const Fraction &left, Fraction right) noexcept;
+	friend Fraction operator*(const Fraction &left, Fraction right) noexcept;
+	friend Fraction operator/(const Fraction &left, Fraction right) throw(std::invalid_argument);
+	friend Fraction& operator+=(Fraction &left, const Fraction &right) noexcept;
+	friend Fraction& operator-=(Fraction &left, const Fraction &right) noexcept;
+	friend Fraction& operator*=(Fraction &left, const Fraction &right) noexcept;
+	friend Fraction& operator/=(Fraction &left, const Fraction &right) throw(std::invalid_argument);
+	friend bool operator>(const Fraction &left, const Fraction &right);
+	friend bool operator<(const Fraction &left, const Fraction &right);
+	friend bool operator==(const Fraction &left, const Fraction &right);
+	friend bool operator>=(const Fraction &left, const Fraction &right);
+	friend bool operator<=(const Fraction &left, const Fraction &right);
+	//operator double(){ return (double)m_iMolecule / m_iDenominator; }
+	static long maxCommonDivisor(long first, long second);
+	static long minCommonMultiple(long first, long second);
+	static void getSimplestFraction(long &molecule, long &denominator) throw(std::invalid_argument);
 };
